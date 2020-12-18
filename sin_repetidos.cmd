cf62c53f8ad4b16cc6748055aec72c4580e27b0e
Francisco Javier Moreno <fmorenono@hotmail.com>
2020-12-18 12:32:52 +0100

    Revision sin repetidos

--- sin_repetidos.cmd
+++ sin_repetidos.cmd
@@ -3,7 +3,7 @@ rem cd..
 rem cd fm
 rem cd caballos
 chcp 65001
-C:\Users\fmore\Anaconda3\python c:\fm\fotocasa\sin_repetidos.py kk_.txt
+C:\Users\fmore\Anaconda3\python c:\fm\fotocasa\sin_repetidos.py kk.txt
 pause
 
 
--- sin_repetidos.py
+++ sin_repetidos.py
@@ -17,13 +17,27 @@ def buscar_id(url3)	:
 
 def guardando_a(fich,valor):
 	try:
-		print("guardando... "+fich)
+		#print("guardando... "+fich)
 		outfile=open(fich,'a')#Indicamoselvalor'w'.
 		outfile.write(valor)
 		outfile.close()
 
 	except:
 		print("Excepcionguardando")
+
+def existe(fich,valor):
+	encontrado=False
+	try:
+		infile=open(fich,'r')
+		for line in list(infile):
+				if (line.find(valor)>-1):
+					encontrado=True
+					
+		infile.close()
+		return encontrado
+	except:
+		print("no existe el fichero")
+
 
 	
 #Programaprincipal
@@ -38,7 +52,13 @@ fichero_aux=fichero_inicial+'.aux'
 z=1
 #while z<1000:
 for line in reversed(list(open(fichero_inicial))):
-	guardando_a(fichero_aux,line.rstrip()+'\n')
+	bloques=line.split(sep=';')
+	bloc=bloques[1]
+	#print("Xxx"+bloc)
+	if not existe(fichero_aux,bloc):
+		guardando_a(fichero_aux,line.rstrip()+'\n')
+	else:
+		print('existe '+bloc)
 										
 print("Acabado")
 #time.sleep(9000)
